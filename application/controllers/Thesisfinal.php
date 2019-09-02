103a137e96ccd0f2e580c61a58cd970c11931542
Chimeraa <achillesfang0@gmail.com>
2019-09-02 17:55:18 +0700

    setting project

---
+++ application/controllers/Thesisfinal.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Thesisfinal extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('javascript');
+	}
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
